d29292faed0c8b828d2ea7bd424f584910864038
Carola Fischer <carola.fischer@kit.edu>
2026-01-25 17:26:10 +0100

    Fix: Naming perun monitor descriptor, change image_size computation to python range

--- scripts/conv2d_scaling_experiment_arbitrary.py
+++ scripts/conv2d_scaling_experiment_arbitrary.py
@@ -1,8 +1,6 @@
 import heat as ht
 import sys
 import os
-from timeit import default_timer as timer
-import csv
 from perun import monitor
 
 
@@ -12,18 +10,24 @@ device = os.getenv("HEAT_DEVICE")
 # define config
 if len(sys.argv) > 1:
     split_image = int(sys.argv[1])
+    d_split_image = split_image
     if split_image < 0:
         split_image = None
+        d_split_image = "nodist"
     split_kernel = int(sys.argv[2])
+    d_split_kernel = split_kernel
     if split_kernel < 0:
         split_kernel = None
+        d_split_kernel = "nodist"
     incr_kernel = int(sys.argv[3])
     start_image = int(sys.argv[4])
     stop_image = int(sys.argv[5])
     kernel_size = int(sys.argv[6])
 else:
     split_image = 0
+    d_split_image = 0
     split_kernel = None
+    d_split_kernel = "nodist"
     incr_kernel = 0
     start_image = 256
     stop_image = 4097
@@ -38,7 +42,7 @@ kernel = ht.random.rand(kernel_size**2, split=split_kernel)
 kernel = ht.reshape(kernel, (kernel_size, kernel_size))
 
 # Iterate through image systes
-image_sizes = ht.arange(start_image,stop_image,start_image, split=None)
+image_sizes = range(start_image,stop_image+1,start_image)
 times = {}
 for c, current_size in enumerate(image_sizes):
     image = ht.random.rand(current_size**2, split=split_image, device=device)
@@ -51,8 +55,9 @@ for c, current_size in enumerate(image_sizes):
     else:
         c_kernel_size = kernel_size
 
+    print (image.shape, kernel.shape)
 
-    @monitor(f"convolve2d_image-{current_size}_kernel-{c_kernel_size}")
+    @monitor(f"convolve2d_{device}{world_size}_image-{d_split_image}-{current_size}_kernel-{d_split_kernel}-{c_kernel_size}")
     def call_convolution2d(signal, kernel):
         convolved_image = ht.convolve2d(signal, kernel, mode="full")
 
