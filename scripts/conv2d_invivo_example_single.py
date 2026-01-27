0a11bf311478bf810db126b0a48963510c0cba05
Carola Fischer <carola.fischer@kit.edu>
2026-01-27 09:44:40 +0100

    Ad:: In vivo experiments to script

---
+++ scripts/conv2d_invivo_example_single.py
@@ -0,0 +1,62 @@
+import heat as ht
+from perun import monitor
+import sys
+import numpy as np
+ht.use_device("cpu")
+
+#monitored functions
+@monitor()
+def conv2d_sobel(image, kernel, mode):
+    y = ht.convolve2d(image, kernel, mode)
+    x = ht.convolve2d(image, kernel.T, mode)
+    return ht.abs(y) + ht.abs(x)
+
+@monitor()
+def conv2d_gauss(image, kernel, mode):
+    return ht.convolve2d(image, kernel, mode)
+
+# Sobel filter
+base_sobel_y_7x7 = [[-3/18, -2/13, -1/10, 0, 1/10, 2/13, 3/18],
+                    [-3/13, -2/8,  -1/5,  0, 1/5,  2/8,  3/13],
+                    [-3/10, -2/5,  -1/2,  0, 1/2,  2/5,  3/10],
+                    [-3/9,  -2,    -1,    0, 1,    2,   3/9],
+                    [-3/10, -2/5,  -1/2,  0, 1/2,  2/5,  3/10],
+                    [-3/13, -2/8,  -1/5,  0, 1/5,  2/8,  3/13],
+                    [-3/18, -2/13, -1/10, 0, 1/10, 2/13, 3/18]]
+
+
+sobel_y_7x7_np = np.array(base_sobel_y_7x7)*20
+sobel_y_7x7 = ht.array(sobel_y_7x7_np, split=None)
+
+# Gaussian filter
+def gaussian_filter(kernel_size, sigma=1, muu=0):
+    # Initializing value of x, y as grid of kernel size in the range of kernel size
+    x, y = np.meshgrid(np.linspace(-1, 1, kernel_size),
+                       np.linspace(-1, 1, kernel_size))
+    dst = np.sqrt(x**2 + y**2)
+
+    # Normal part of the Gaussian function
+    normal = 1 / (2 * np.pi * sigma**2)
+
+    # Calculating Gaussian filter
+    gauss = np.exp(-((dst - muu)**2 / (2.0 * sigma**2))) * normal
+
+    return gauss  # Return the calculated Gaussian filter
+
+gauss_9x9 = ht.array(gaussian_filter(9), split=None)
+# data
+
+data_path = "/lsdf/kit/scc/projects/himalaya/Data/Data/hdf5/CT_P0001.h5"
+set_name = "CT/volume"
+
+single_im =ht.load_hdf5(data_path, set_name, split=1, slices=(slice(1499,1501), None, None))
+single_im = ht.squeeze(single_im[0,:,:])
+
+
+# Experiments single_im
+for i in range(50):
+    edge_det = conv2d_sobel(single_im, sobel_y_7x7_np, mode="same")
+    blurr_im = conv2d_gauss(single_im, gauss_9x9, mode="same")
+
+ht.save_hdf5(edge_det,"images_manipulation.h5", "sobel7x7", "w")
+ht.save_hdf5(blurr_im, "images_manipulation.h5", "gauss9x9", "a")
